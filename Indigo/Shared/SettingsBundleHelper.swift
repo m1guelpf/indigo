aa2f6d4aa6acc9aeaf143f0be835c6e0910d4b7c
Miguel Piedrafita <github@miguel.build>
2024-02-01 04:08:15 +0000

    whoops
    Co-Authored-By: Nick <nickdobos@gmail.com>

--- Indigo/Shared/SettingsBundleHelper.swift
+++ Indigo/Shared/SettingsBundleHelper.swift
@@ -19,9 +19,9 @@ class SettingsBundleHelper {
 
 	class func setVersionAndBuildNumber() {
 		let version: String = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
-		UserDefaults.standard.set(version, forKey: "version_preference")
+		UserDefaults.standard.set(version, forKey: SettingsBundleKeys.AppVersionKey)
 
 		let build: String = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String
-		UserDefaults.standard.set(build, forKey: "build_preference")
+		UserDefaults.standard.set(build, forKey: SettingsBundleKeys.BuildVersionKey)
 	}
 }
